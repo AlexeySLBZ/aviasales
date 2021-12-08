128fbf4e5413a423f8788a23f867a6c8d1908776
Alexey Sulaberidze <alexeyslbz@gmail.com>
2021-12-09 02:02:15 +0400

    Updates

---
+++ precache-manifest.6bc4fcbaf3835c921eebd73022e97b09.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "798d983bfb64432def6d9defe38eedcb",
+    "url": "/aviasales/index.html"
+  },
+  {
+    "revision": "0bb52192e002bf001aef",
+    "url": "/aviasales/static/css/2.bcaaf67b.chunk.css"
+  },
+  {
+    "revision": "00c01def7ab294703be7",
+    "url": "/aviasales/static/css/main.9654c544.chunk.css"
+  },
+  {
+    "revision": "0bb52192e002bf001aef",
+    "url": "/aviasales/static/js/2.5df035e1.chunk.js"
+  },
+  {
+    "revision": "63b5bbb9f56ac8602173d80508d3cb63",
+    "url": "/aviasales/static/js/2.5df035e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "00c01def7ab294703be7",
+    "url": "/aviasales/static/js/main.1de2ae47.chunk.js"
+  },
+  {
+    "revision": "0cc15852d7adf8a9722e",
+    "url": "/aviasales/static/js/runtime-main.b4f11597.js"
+  },
+  {
+    "revision": "3fe3a28912fafcb7349298274004fead",
+    "url": "/aviasales/static/media/LogoAirplan.3fe3a289.svg"
+  },
+  {
+    "revision": "629a55a7e793da068dc580d184cc0e31",
+    "url": "/aviasales/static/media/OpenSans-Regular.629a55a7.ttf"
+  },
+  {
+    "revision": "35aae663c7b89bf47c1cd86506e6b9cd",
+    "url": "/aviasales/static/media/S7 Logo.35aae663.svg"
+  },
+  {
+    "revision": "68ca57dc522eda771a31bb07bdd256b1",
+    "url": "/aviasales/static/media/Shape.68ca57dc.svg"
+  }
+]);
